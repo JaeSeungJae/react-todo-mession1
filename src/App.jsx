b77e7ec4592a6c01b53e120281a1cc9f9dd2c3e8
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-07-08 10:13:14 +0900

    fix: 주석 수정 / import 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import React, { useState, useEffect } from "react";
-import TodoItem from "./components/TodoItem";
 import TodoList from "./components/TodoList";
-import { useTodos } from "./hooks/useTodos";
 import TodoWriteForm from "./components/TodoWriteForm";
 import { TodosProvider } from "./context/TodosContext.jsx";
 
 function App() {
-  // const { todos, handleSubmit, removeTodo, onToggle } = useTodos();
-
   return (
     <>
       <TodosProvider>
